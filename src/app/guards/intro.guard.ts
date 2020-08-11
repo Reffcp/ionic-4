ebe552f43f7b7b90483fe22b7df913334c7adc71
daff <daffmg1@gmail.com>
2020-08-11 07:35:39 -0500

    uso de guardianes para navegación

---
+++ src/app/guards/intro.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Storage } from '@ionic/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IntroGuard implements CanActivate {
+  constructor(private storage:Storage, private router:Router){}
+  async canActivate() {
+      const inIntroShowed=await this.storage.get('isIntroShowed')
+      if (inIntroShowed) {
+        return true;
+      } else {
+        this.router.navigateByUrl('/intro')
+      }
+  }
+  
+}
