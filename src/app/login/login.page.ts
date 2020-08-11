bd9dc6798861fcaf5680bde8d797b56f6c49302a
daff <daffmg1@gmail.com>
2020-08-11 07:56:09 -0500

    pagina de login creando un formulario reactivo

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm= formBuilder.group({
+      email: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.email
+        ])), 
+      
+    })
+  }
 
   ngOnInit() {
   }
