35bc0186e45e42a4e6a444110dac96c499db7b4c
daff <daffmg1@gmail.com>
2020-08-11 07:06:59 -0500

    implementando directiva ngFor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,6 +12,27 @@ export class HomePage {
     centerSlides: true,
     speed: 400
   }
+
+  slides= [
+    {
+      tittle:'App musical',
+      subTitle:'Reproduce contenido',
+      description:'Por: Cristian Covarrubias',
+      icon:'play'
+    },
+    {
+      tittle:'Video integrado',
+      subTitle:'Reproductor de video',
+      description:'si, como spotify',
+      icon:'videocam-outline'
+    },
+    {
+      tittle:'Comenzamos',
+      subTitle:'',
+      description:'',
+      icon:'play'
+    },
+]
   constructor() {}
 
 }
