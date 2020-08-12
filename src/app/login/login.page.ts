40e21af19c091243c248eed5105a61647c0c1e55
daff <daffmg1@gmail.com>
2020-08-12 06:24:23 -0500

    manejo de errores en formularios reactivos

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,15 +8,31 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 })
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
+  validationMessage={
+    email:[
+      {type:"required", message:"El email es requerido"},
+      {type:"pattern", message:"Formato de email incorrecto"},
+    ],
+    password:[
+      {type:"required", message:"El password es requerido"},
+      {type:"minlength", message:"El password debe ser de minimo 5 caracteres"},
+    ]
+  }
   constructor(private formBuilder: FormBuilder) {
-    this.loginForm= formBuilder.group({
+    this.loginForm = formBuilder.group({
       email: new FormControl(
         "",
         Validators.compose([
           Validators.required,
           Validators.email
-        ])), 
-      
+        ])),
+      password: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(5)
+        ])),
+
     })
   }
 
